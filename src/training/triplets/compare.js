21ed33c9e2dab54eeba41b23d8662607a4a7499e
Luciano Callero <lucianocallero@gmail.com>
2019-04-06 23:48:53 -0300

    Triplets Ok.

--- src/training/triplets/compare.js
+++ src/training/triplets/compare.js
@@ -1,10 +1,17 @@
-
+'use strict'
 function compareTriplets (a, b) {
-  let result = []
-
-  return result
+  let scores = [0, 0]
+  a = a.split(' ').map(item => parseInt(item))
+  b = b.split(' ').map(item => parseInt(item))
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+      ++scores[0]
+    } else if (a[i] < b[i]) {
+      ++scores[1]
+    }
+  }
+  return scores
 }
-
 module.exports = {
   compareTriplets
 }
--- src/training/triplets/test.js
+++ src/training/triplets/test.js
@@ -1,12 +1,24 @@
 'use strict'
 /* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 const chai = require('chai')
+const expect = require('chai').expect
 const should = require('chai').should()
+const deepEqualInAnyOrder = require('deep-equal-in-any-order')
 const compare = require('./compare')
+chai.use(deepEqualInAnyOrder)
 
 describe('Should test Triplets comparing code.', () => {
   it('should return an array', async () => {
     let result = compare.compareTriplets('1 2 3', '3 2 1')
     result.should.be.an('array')
+  })
+  it('should return an array equal to [1,1]', async () => {
+    let result = compare.compareTriplets('1 2 3', '3 2 1')
+    expect(result).to.deep.equalInAnyOrder([1, 1])
+  })
+  it('should return an array equal to [0,3]', async () => {
+    let result = compare.compareTriplets('0 0 0', '1 1 1')
+    expect(result).to.deep.equalInAnyOrder([0, 3])
   })
 })
