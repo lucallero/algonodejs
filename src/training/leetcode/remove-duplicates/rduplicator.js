555e0e5abfe1cea0fb6a613a147e08d5a26ed8f9
Luciano Callero <lucianocallero@gmail.com>
2019-04-10 15:15:10 -0300

    Refactor, spliting hackrank leetcode

---
+++ src/training/leetcode/remove-duplicates/rduplicator.js
@@ -0,0 +1,12 @@
+'use strict'
+function removeDuplicates (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i, 1)
+      --i
+    }
+  }
+  return nums.length
+}
+
+module.exports = { removeDuplicates }
