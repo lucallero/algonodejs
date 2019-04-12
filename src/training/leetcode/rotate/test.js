3359457fcb2798de08d89d7021b391aa3bf461ff
Luciano Callero <lucianocallero@gmail.com>
2019-04-12 00:55:46 -0300

    Rotating refactor (lint standard js)

--- src/training/leetcode/rotate/test.js
+++ src/training/leetcode/rotate/test.js
@@ -9,17 +9,17 @@ const rotator = require('./rotator')
 chai.use(deepEqualInAnyOrder)
 
 describe('Should test remove duplicates  function.', () => {
-    it('should return an Array equals [5,6,7,1,2,3,4]', async () => {
-        let result = rotator.rotate([1, 2, 3, 4, 5, 6, 7], 3)
-        result.should.deep.equalInAnyOrder([5, 6, 7, 1, 2, 3, 4])
-    })
-    it('should return an Array equals [7,1,2,3,4,5,6]', async () => {
-        let result = rotator.rotate([1, 2, 3, 4, 5, 6, 7], 1)
-        result.should.deep.equalInAnyOrder([7, 1, 2, 3, 4, 5, 6])
-    })
+  it('should return an Array equals [5,6,7,1,2,3,4]', async () => {
+    let result = rotator.rotate([1, 2, 3, 4, 5, 6, 7], 3)
+    result.should.deep.equalInAnyOrder([5, 6, 7, 1, 2, 3, 4])
+  })
+  it('should return an Array equals [7,1,2,3,4,5,6]', async () => {
+    let result = rotator.rotate([1, 2, 3, 4, 5, 6, 7], 1)
+    result.should.deep.equalInAnyOrder([7, 1, 2, 3, 4, 5, 6])
+  })
 
-    it('should return an Array equals [3, 99, -1, -100]', async () => {
-        let result = rotator.rotate([-1, -100, 3, 99], 2)
-        result.should.deep.equalInAnyOrder([3, 99, -1, -100])
-    })
+  it('should return an Array equals [3, 99, -1, -100]', async () => {
+    let result = rotator.rotate([-1, -100, 3, 99], 2)
+    result.should.deep.equalInAnyOrder([3, 99, -1, -100])
+  })
 })
